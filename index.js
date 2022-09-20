a28b50006b1187b1031a18e8b319efc2d5f37203
Leiner Barrios Medina <leinerbarrios99.ljbm@gmail.com>
2022-09-19 21:19:29 -0500

    Define routes

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+// Crear servidor, aplicación de express
+const app = express();
+
+// Rutas
+app.use( '/api/auth', require('./routes/auth') )
+
+app.listen(4000, () => {
+    console.log(`Servidor corriendo en el puerto ${4000}`);
+})
